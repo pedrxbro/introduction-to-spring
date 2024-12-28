37db803a987a9c97f170914fa7ab20508890b480
pedrxbro <pedrohdpaula@edu.univali.br>
2024-12-28 10:14:09 -0300

    Exception Handler - FindById

---
+++ src/main/java/com/univali/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.univali.course.resources.exceptions;
+
+
+import com.univali.course.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.catalina.filters.ExpiresFilter;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Recurso não encontrado";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/univali/course/services/UserService.java
+++ src/main/java/com/univali/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.univali.course.services;
 
 import com.univali.course.entities.User;
 import com.univali.course.repositories.UserRepository;
+import com.univali.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     public User insert (User obj) {
         return repository.save(obj);
