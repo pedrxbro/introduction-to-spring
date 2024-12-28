25a6110ac0ad6d08b0dad2e2dcc1c57f95b2edcb
pedrxbro <pedrohdpaula@edu.univali.br>
2024-12-28 08:58:31 -0300

    Product-OrderItem oneToMany association

--- src/main/java/com/univali/course/entities/Product.java
+++ src/main/java/com/univali/course/entities/Product.java
@@ -29,6 +29,10 @@ public class Product  implements Serializable {
     inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
+
     public Product() {};
     public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -80,6 +84,15 @@ public class Product  implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem item : items) {
+            set.add(item.getOrder());
+        }
+        return set;
     }
 
     @Override
